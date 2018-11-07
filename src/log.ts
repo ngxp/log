50fa72ad2de3169c2e8232f50b3157947c5f9c94
Marvin Luchs <marvin.luchs@holisticon.de>
2018-11-07 08:47:52 +0100

    fix: add typing for getLogger to fix build issue

--- src/log.ts
+++ src/log.ts
@@ -1,7 +1,8 @@
 import { LogManager } from './log-manager';
+import { Logger } from './logger';
 
 const defaultLogManager = new LogManager();
 
-export function getLogger(name?: string) {
+export function getLogger(name?: string): Logger {
     return defaultLogManager.getLogger(name);
 }
