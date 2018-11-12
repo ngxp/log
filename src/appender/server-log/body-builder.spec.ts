e8587e5244ed6014b7260a225b68ca42a6aba6af
Marvin Luchs <marvin.luchs@holisticon.de>
2018-11-12 11:02:03 +0100

    feat: update default body builder to include logger name in message

--- src/appender/server-log/body-builder.spec.ts
+++ src/appender/server-log/body-builder.spec.ts
@@ -1,10 +1,40 @@
-import { logMessages } from '../../test';
+import { LogMessage } from '../../log-message';
+import { grandChildLoggerName, logMessage, rootLoggerName } from '../../test';
 import { defaultBodyBuilder } from './body-builder';
 
 describe('bodyBuilder', () => {
-    describe('defaultBodyBuiler', () => {
+    describe('defaultBodyBuilder', () => {
+        const rootMessage: LogMessage = {
+            ...logMessage,
+            loggerName: rootLoggerName
+        };
+        const expectedRootMessage = logMessage.message;
+
+        const childMessage: LogMessage = {
+            ...logMessage,
+            loggerName: grandChildLoggerName
+        };
+        const expectedChildMessage = `${grandChildLoggerName.substr(1)} ${logMessage.message}`;
+
         it('stringifies the array of log messages', () => {
-            expect(defaultBodyBuilder(logMessages)).toBe(JSON.stringify(logMessages));
+            expect(defaultBodyBuilder([rootMessage, childMessage])).toBe(JSON.stringify([
+                { level: rootMessage.level, message: expectedRootMessage },
+                { level: childMessage.level, message: expectedChildMessage }
+            ]));
+        });
+
+        it('formats a log message from a root logger without  the logger name', () => {
+            expect(defaultBodyBuilder([rootMessage])).toBe(JSON.stringify([{
+                level: rootMessage.level,
+                message: expectedRootMessage
+            }]));
+        });
+
+        it('formats a log message from a child logger with the logger name', () => {
+            expect(defaultBodyBuilder([childMessage])).toBe(JSON.stringify([{
+                level: childMessage.level,
+                message: expectedChildMessage
+            }]));
         });
     });
 });
--- src/appender/server-log/body-builder.ts
+++ src/appender/server-log/body-builder.ts
@@ -1,5 +1,18 @@
+import { isEmpty } from 'lodash-es';
 import { LogMessage } from '../../log-message';
 
 export type BodyBuilder = (logMessages: LogMessage[]) => string;
 
-export const defaultBodyBuilder: BodyBuilder = (logMessages: LogMessage[]) => JSON.stringify(logMessages);
+export const defaultBodyBuilder: BodyBuilder = (logMessages: LogMessage[]) => {
+    const messages = logMessages
+        .map(message => ({
+            level: message.level,
+            message: [
+                message.loggerName.substr(1),
+                message.message
+            ]
+                .filter(part => !isEmpty(part))
+                .join(' ')
+        }));
+    return JSON.stringify(messages);
+};
